54a095467d0db67870e53d9b2b6ada6e8b3a6777
superiorkid <bayiPetani@protonmail.com>
2022-03-25 00:27:06 +0800

    fetch data from blog

--- app/feed.py
+++ app/feed.py
@@ -1,19 +1,54 @@
 import feedparser
 import dateutil.parser
 
-urls = [
-  "https://www.geeksforgeeks.org/feed/",
-  "https://dev.to/feed",
-  "https://joelhooks.com/rss.xml"
-]
+class BlogFeed:
 
-feeds = [feedparser.parse(url)['entries'] for url in urls]
+  def __init__(self):
+    self.__programming = {
+      "https://www.geeksforgeeks.org/feed/",
+      "http://feeds.feedburner.com/CssTricks",
+      "http://feeds.dzone.com/home",
+      "https://sdtimes.com/feed/",
+      "https://scand.com/company/blog/feed/"
+    }
 
-feed = [item for feed in feeds for item in feed]
-feed.sort(key=lambda x: dateutil.parser.parse(x['published']), reverse=True)
+    self.__opensource = {
+      "https://www.cyberciti.biz/feed/",
+      "https://itsfoss.com/feed/",
+      "https://linuxhint.com/feed/",
+      "https://ostechnix.com/feed/",
+      "https://www.fosslinux.com/feed"
+    }
 
-f = [{'title': i['title'], 'date': i['published'], 'link': i['link']} for i in feed[:10]]
+  @property
+  def programming(self):
+    return self.__programming
 
-print(f)
+  @property
+  def opensource(self):
+    return self.__opensource
 
+  def all_feed(self):
+    url_merge = self.__opensource | self.__programming
+
+    blog = [feedparser.parse(url) for url in url_merge]
+    temp = []
+
+    for i in blog:
+      if 'image' in i.feed:
+        temp.append({'blog_title': i.feed.title, 'blog_img': i.feed.image.href})
+      else:
+        temp.append({'blog_title': i.feed.title, 'blog_img': None})
+
+    feeds = [feed.entries for feed in blog]
+    feed = [item for feed in feeds for item in feed]
+    feed.sort(key=lambda x: dateutil.parser.parse(x.['published']), reverse=True)
+
+    res = [{'title': i['title'], 'date': i['published'], 'link': i['link']} for i in feed[:6]]
+
+    return
+
+
+feed1 = BlogFeed()
+print(feed1.all_feed())
 
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,35 @@ from flask import render_template
 
 @app.get('/')
 def index():
+  """
+    front view berisikan gabungan dari semua feed
+  """
 
   return render_template('index.html', title='Front view')
 
+
+@app.get('/programming')
+def programming():
+  """
+    programming section
+  """
+  pass
+
+
+@app.get('/opensource')
+def open_source():
+  """
+    open source section
+  """
+  pass
+
+
+@app.get('/programming/geekforgeeks')
+def geek_for_geeks():
+  """
+    geek for geeks section
+  """
+  pass
+
+
+# etc
