0fb56a71a4062a8e10c722ed326e67bb72331e9b
superiorkid <bayiPetani@protonmail.com>
2022-03-26 07:33:20 +0800

    Better routing method

--- app/feed/views.py
+++ app/feed/views.py
@@ -1,5 +1,20 @@
 from . import feed
+
 from flask import render_template
+from app.feeds import BlogFeeds
+
+
+@feed.get('/all')
+def front_view():
+  """
+    front view berisikan gabungan dari semua feed
+  """
+
+  blog = BlogFeeds()
+  data = blog.all_feeds()
+
+  return render_template('index.html',data = data, title = 'Front view')
+
 
 
 @feed.get('/programming')
--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,9 @@
+from app.feeds import BlogFeeds
 from . import main
-from flask import render_template
-from ..feed import BlogFeed
+from flask import redirect, url_for
 
 
 @main.get('/')
 def index():
-  """
-    front view berisikan gabungan dari semua feed
-  """
-
-  blog = BlogFeed()
-  data = blog.all_feeds()
-
-  return render_template('index.html',data = data, title = 'Front view')
+  # return redirect('/blog/all')
+  return redirect(url_for('feed.front_view'))
