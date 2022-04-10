c31e2ca09a1f3c5f58911b2f305014a29b533cc0
superiorkid <bayiPetani@protonmail.com>
2022-04-10 09:48:41 +0800

    user login db models

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(64), index=True, nullable=False)
+  email = db.Column(db.String(80), index=True, nullable=False)
+  password_hash = db.Column(db.String(128))
+
+  def __repr__(self):
+    return '<User {}>'.format(self.username)
+
+  @property
+  def password(self):
+    raise AttributeError('password is not a readable attribute')
+
+  @password.setter
+  def password(self, pasword):
+    self.password_hash = generate_password_hash(password)
+
+  def verify_password(self, password):
+    return check_password_hash(self.password_hash, password)
