42b864ee4e9598ea4c7cb312cdc547d43c7e2f6f
superiorkid <bayiPetani@protonmail.com>
2022-04-10 09:48:12 +0800

    config

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+  SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(12)
+
+  # database
+  SQLALCEHMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
